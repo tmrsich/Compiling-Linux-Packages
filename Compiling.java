8f1f85eb943f9622e6cf0556740cd9d566fb436b
Thomas Mrsich <thomrs24@bergen.org>
2023-08-06 18:58:46 -0400

    Initializes empty strings to store package names temporarily

--- Compiling.java
+++ Compiling.java
@@ -3,9 +3,21 @@ public class Compiling {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Takes user input
-        System.out.println("Enter the list of packages separated by a space");
-        String userInput = input.nextLine().toLowerCase();
+        // Initializes empty strings to store user input
+        String userInput = "";
+        String inputLines = "";
+
+        // Keeps taking input until "done" is entered
+        while (true) {
+            System.out.println("Enter a package name, or type 'done' when finished");
+            inputLines = input.nextLine().toLowerCase(); // Changes any package names to lower case
+
+            if (inputLines.equals("done")) {
+                break; // Exits the while loop when "done" is entered
+            }
+
+            userInput += inputLines + " "; // Appends the entered package name
+        }
 
         // Splits the user input into an array of package names
         String[] packageList = userInput.split(" ");
